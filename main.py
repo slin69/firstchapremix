7f283937f8f04530c114b8612545349809c085d7
slin69 <83825913+slin69@users.noreply.github.com>
2022-01-22 09:25:07 -0600

    another one

--- main.py
+++ main.py
@@ -33,5 +33,5 @@ def is_typing(data):
     socketio.emit("is_typing",data,broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app,debug=True,host='0.0.0.0', port=5004)
+    socketio.run(app,host='0.0.0.0', port=5000)
     #socketio.run(app,debug=True)
