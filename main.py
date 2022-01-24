9146e20d8030d2b2affcdf3167f96fd9ffa1a5b3
slin69 <83825913+slin69@users.noreply.github.com>
2022-01-23 20:28:41 -0600

    shit

--- main.py
+++ main.py
@@ -34,5 +34,6 @@ def is_typing(data):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app,host='0.0.0.0', port=5004)
+    app.run(debug=True)
+    #socketio.run(app,host='0.0.0.0', port=5004)
     #socketio.run(app,debug=True)
