5546db0795bee09344c453ad1b48524319bf6ee3
slin69 <83825913+slin69@users.noreply.github.com>
2022-01-24 15:48:47 -0600

    shit

--- main.py
+++ main.py
@@ -1,39 +1,12 @@
-from website import create_app
-from flask_socketio import *
-
-from website.views import *
-users=[]
-app=create_app()
-socketio = SocketIO(app,cors_allowed_origins="*")
-
-
-
+from flask import Flask, render_template, make_response, redirect
+from flask_socketio import SocketIO, send, emit
+app = Flask(__name__)
+socketio = SocketIO(app)
+@app.route('/')
+def index():
+    return render_template('chat.html')
 @socketio.on("message")
-def handle_message(message):
-    username=message["username"]
-    msg=message["message"]
-    new_message=f"{username}: {msg}"
-
-    type=message["type"]
-    if type=="message":
-        socketio.emit("message",new_message,broadcast=True)
-    if type=="connect":
-        users.append(username)
-
-        socketio.emit("message",new_message,broadcast=True)
-        socketio.emit("update_users",users,broadcast=True)
-    if type=="disconnect":
-        users.remove(username)
-
-        socketio.emit("message",new_message,broadcast=True)
-        socketio.emit("update_users",users,broadcast=True)
-@socketio.on("is_typing")
-def is_typing(data):
-
-    socketio.emit("is_typing",data,broadcast=True)
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True)
-    #socketio.run(app,host='0.0.0.0', port=5004)
-    #socketio.run(app,debug=True)
+def handleMessage(data):
+    emit("new_message",data,broadcast=True)
+if __name__ == "__main__":
+    socketio.run(app, debug=True, port=5004)
